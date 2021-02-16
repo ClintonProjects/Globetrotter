4396bda95254291b679c48fe44e1b84bf184f595
Evan Campion <evancn7@gmail.com>
2021-02-16 10:22:53 +0000

    initial priject set up with nodejs and base app files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">Team 8
+      </div>
+    );
+  }
+}
+export default App;
