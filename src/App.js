600fca22d476a712798d29bba62b5a43f3d426b6
Kim <64961619+kimwinters82@users.noreply.github.com>
2021-02-22 14:07:36 +0000

    Update App.js
    Testing push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">Team 8
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "App" > Team 8 test <
+            /div>
+        );
+    }
 }
 export default App;
