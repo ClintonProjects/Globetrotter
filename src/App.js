9ab32a070d09b002952dbaafd8fa94c265224fea
Kim <64961619+kimwinters82@users.noreply.github.com>
2021-02-22 16:13:32 +0000

    Update App.js
    test test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
     render() {
-        return (
-            <div className="App" > Team 8 test test
-            </div>
+        return ( <
+            div className = "App" > Team 8 test test test <
+            /div>
         );
     }
 }
