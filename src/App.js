7bcb59b9f59c9f65e50bd6a99790c626f878cb44
Evan Campion <evancn7@gmail.com>
2021-02-22 16:08:39 +0000

    remove css import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
     render() {
-        return ( <
-            div className = "App" > Team 8 test <
-            /div>
+        return (
+            <div className="App" > Team 8 test test
+            </div>
         );
     }
 }
