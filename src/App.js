ebebaef32c18ed485186f76ba348b1a237dc1219
AnnaKasprzak2021 <79107236+AnnaKasprzak2021@users.noreply.github.com>
2021-02-22 18:06:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Login from "./components/login/Login";
+import Login from "./components/login/login";
 
 class App extends Component {
     render() {
