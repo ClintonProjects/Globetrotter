4ad4e0c65a01a497207ddef1f182a3e905adb97c
anna <anna@anna.com>
2021-02-25 23:37:14 +0000

    merged master into About branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Login from "./components/login/login";
 import About from "./components/about/About";
+import NavBar from "./components/NavBar/NavBar";
 class App extends Component {
     render() {
         return ( 
         <div className = "App" > Team 9 test 
+        <NavBar/>
         <Login/>
 
         <About></About>
