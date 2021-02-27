761f37701163737ed38efa6e8e0eb0c6381011e0
evancn7 <evancn7@gmail.com>
2021-02-27 11:12:59 +0000

    add componentDidMount to instantiate map, provide definition and set projection type

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -4,6 +4,15 @@ import * as am4maps from "@amcharts/amcharts4/maps";
 import geodata from "@amcharts/amcharts4-geodata/worldLow";
 
 class Map extends Component {
+    componentDidMount(){
+        // instantiate the map object
+        let map = am4core.create("map", am4maps.MapChart);
+
+        // provide the map object with a definition (GEOJSON)
+        map.geodata = geodata;
+
+        // set the map projection type
+        map.projection = new am4maps.projections.NaturalEarth1();
   render() {
     return (
         <>
