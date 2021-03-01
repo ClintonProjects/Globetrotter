268219304a0d0276bcd935482dab6edd0da68d40
anna <anna@anna.com>
2021-03-01 14:38:08 +0000

    added contactus and help. Also improved about.

---
+++ src/components/contactUs/ContactUs.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import "./ContactUs.css";
+
+class ContactUs extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      subject: '',
+      description: ''};
+
+    //binding the event handler methods
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangeSubject = this.handleChangeSubject.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+  handleChangeSubject(event) {
+    this.setState({subject: event.target.value});
+  }
+  handleChangeDescription(event) {
+    this.setState({description: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An email was submitted: ' + this.state.email + ' with subject: ' + this.state.subject + 
+    ' with description: ' + this.state.description);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        
+      <div className="content1">
+        <h3> Contact us </h3>
+        <br/><br/>
+        <h5> Submit a request: </h5>
+        <form onSubmit={this.handleSubmit} className="form1">
+          <label>
+          Your email address:
+            <br/>
+            <input type="text" className="input1" value={this.state.email} onChange={this.handleChangeEmail} />
+          </label>
+          <br/>
+          <label>
+          Subject:
+            <br/>
+            <input type="text" value={this.state.subject} onChange={this.handleChangeSubject} />
+          </label>
+          <br/>
+          <label>
+          Description:
+            <br/>
+            <textarea value={this.state.description} onChange={this.handleChangeDescription} />
+          </label>
+
+
+          <br/><br/>
+          <input type="submit" value="Submit" />
+        </form>
+            
+            
+      </div>
+    );
+  }
+}
+export default ContactUs;
