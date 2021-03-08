4e6add530c1f8ecd78f7a4549f6e32040571877c
ClintonProjects <cbates11@live.ie>
2021-03-08 15:59:06 +0000

    Pre-Login-Homepage
    Pre-Login-Homepage

---
+++ src/components/Pre-LoginMap/RegisterMessage.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./RegisterMessage.css";
+import Map from './Map';
+// import cat from './src/';
+
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+export class RegisterMessage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div class="row text-center ">
+        <div class="col-sm-12 loginMessage bg-danger text-white warning-borader">
+          <Router>
+            <Switch>
+              <h5>Register to unlock all featrues, Please login Click to login </h5>
+            </Switch>
+          </Router>
+        </div>
+        <Router>
+          <Switch>
+            <div onClick={() => null}>
+              <Map />
+            </div>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default RegisterMessage;
