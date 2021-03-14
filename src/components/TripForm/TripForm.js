14b4d5ceefeb2fa78ebeb84bc0c7f99c75941dd1
Evan <evancn7@gmail.com>
2021-03-14 14:36:06 +0000

    add form for adding trip to user database

---
+++ src/components/TripForm/TripForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./TripForm.css";
+
+class TripForm extends Component {
+  render() {
+    return (
+      <form class="trip-form">
+        <h1>Your Trip</h1>
+        <div id="inputContainer">
+          <input
+            type="text"
+            id="country"
+            name="country"
+            placeholder="Enter Country"
+          />
+          <input
+            type="text"
+            id="startdate"
+            name="startdate"
+            placeholder="Enter Start Date"
+          />
+          <input
+            type="text"
+            id="enddate"
+            name="enddate"
+            placeholder="Enter End Date"
+          />
+        </div>
+        <div id="buttonContainer">
+          <input type="submit" id="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default TripForm;
