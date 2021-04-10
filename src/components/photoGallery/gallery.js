a5d586fd88f1b6c59d91fff00d90309924e27895
anna <anna@anna.com>
2021-04-10 18:17:49 +0100

    Carousel select event handling. Thumbnail selection linked to carousel active picture.

--- src/components/photoGallery/gallery.js
+++ src/components/photoGallery/gallery.js
@@ -14,14 +14,38 @@ class Gallery extends Component{
     
         this.state = {
           docs: [],
-          selectedThumbnail: -1 //use this state to keep track which thumbail is selected
+          selectedThumbnail: -1, //use this state to keep track which thumbail is selected
+          carouselIndex: 0, //store the active carousel index in the state so that it can be programaticaly changed
         };
     }
     
     //method to handle thumbnail clicks
     thumbnailClick = (event) => {
+        let docID = event.currentTarget.getAttribute('docID');
         //set selected thumbnail pic id to state
-        this.setState({selectedThumbnail: event.currentTarget.getAttribute('docID')});
+        this.setState({selectedThumbnail: docID});
+        //use the thumbnail id to select the big picture on the carousel
+        if(this.state.docs.length >0){
+            //find the index of clicked thumbnail
+            let idx = this.state.docs.findIndex(doc => doc.id == docID)
+            //set the carousel active index to the one of selected thumbnail 
+            this.setState({carouselIndex: idx});
+        }
+        
+    }
+
+    //method the carousel will use on each transition
+    carouselSelect = (selectedIndex) => {
+        //set the carousel active index
+        this.setState({carouselIndex: selectedIndex});
+        //check if any pictures have been loaded from firestore
+        if(this.state.docs.length >0){
+            //get the doc at selected index by carousel animation 
+            let carouselDocId = this.state.docs[selectedIndex].id;
+            //set the selected thumbnail id to the newly retrieved doc
+            this.setState({selectedThumbnail: carouselDocId});
+        }
+
     }
 
     render(){
@@ -76,7 +100,8 @@ class Gallery extends Component{
                 Load Photos </Button>
             </Row>
             <Row className="pb-2">
-                <Carousel> {cItems} </Carousel>
+                <Carousel pause="hover" interval={2000} activeIndex={this.state.carouselIndex} onSelect={this.carouselSelect}> 
+                {cItems} </Carousel>
             </Row>
             <Row className="no-gutters">
                 {tItems}                 
