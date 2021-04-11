7f5ad0d670720f8fb1de97e70e48bb3a52f9a260
Evan Campion <evancn7@gmail.com>
2021-04-11 11:26:04 +0100

    remove unused code

--- src/views/PreLoginMap/Preloginmap.js
+++ src/views/PreLoginMap/Preloginmap.js
@@ -1,31 +1,21 @@
-import React, { Component } from 'react';
-import RegisterMessage from '../../components/Pre-LoginMap/RegisterMessage';
-// import Map from '../../components/Map/Map.js';
-import { Route } from 'react-router';
-import Footer from '../../components/footer/Footer.js';
-import NavBar from '../../components/NavBar/NavBar.js';
-import DemoMap from './demo_map.png';
+import React, { Component } from "react";
+import RegisterMessage from "../../components/Pre-LoginMap/RegisterMessage";
+import { Route } from "react-router";
+import DemoMap from "./demo_map.png";
 
-import {
-    BrowserRouter as Router,
-    Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class Preloginmap extends Component {
-    render() {
-        //const authenticated = this.props.authenticated;
-        //console.log(authenticated);
-        return (
-            <div>
-                {/* {!authenticated && ( */}
-                    <RegisterMessage />
-                {/* )} */}
-                {/* {!authenticated && ( */}
-                    <Link to="/login"><img src={DemoMap} alt="" /></Link>
-                {/* )} */}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <RegisterMessage />
+        <Link to="/login">
+          <img src={DemoMap} alt="" />
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Preloginmap;
